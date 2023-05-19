f529da269b82645bba2b3c172975ed3cebd90b84
ifouzezi <ifoghaleuzezi@gmail.com>
2023-05-19 10:55:50 +0100

    Update 1-print_square.js

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -5,18 +5,17 @@
     of the program.
 */
 
-
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1)
+    process.exit(1);
 }
 
-size = parseInt(process.argv[2], 10)
+const squareSize = parseInt(process.argv[2], 10);
 
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
+for (let row = 0; row < squareSize; row++) {
+    for (let column = 0; column < squareSize; column++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
