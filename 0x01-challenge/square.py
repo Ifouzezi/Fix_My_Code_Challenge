33b8415c5d263d7d98ac6d0e5c51cc683022455d
ifouzezi <ifoghaleuzezi@gmail.com>
2023-07-10 07:30:32 +0100

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 
 class square():
-
+    
     width = 0
     height = 0
 
-
+    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.height * self.width
+        return self.width * self.width
 
     def PermiterOfMySquare(self):
         return (self.width * 2) + (self.height * 2)
